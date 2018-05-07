2d67780afc16a65b676ea61e9401735106b2924a
PPAmaranth <523002128@qq.com>
2018-05-07 16:48:15 +0800

    Signed-off-by: PPAmaranth <523002128@qq.com>

---
+++ src/components/main.vue
@@ -0,0 +1,20 @@
+<template>
+	<top></top>
+</template>
+
+<script>
+
+import top from '@/components/top'
+export default {
+  name: 'main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      visible: false 
+    }
+  },
+  components: {
+		top
+	}
+}
+</script>
---
+++ src/components/top.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>123333</div>
+</template>
+
+<script>
+export default {
+  name: 'top',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      visible: false 
+    }
+  }
+}
+</script>
